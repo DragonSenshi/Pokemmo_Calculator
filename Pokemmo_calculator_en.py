8efb19e396dcfa67c98c716221d4a6c3066caef3
DragonSenshi <151685563+DragonSenshi@users.noreply.github.com>
2023-11-21 15:52:35 -0600

    Add files via upload

--- Pokemmo_calculator_en.py
+++ Pokemmo_calculator_en.py
@@ -1,108 +1,101 @@
 print("***********************************************************")
-print("* Bienvenido a la calculadora de costos para cría pokemmo *")
+print("* Welcome to the Pokemmo Breeding Cost Calculator *")
 print("***********************************************************")
 
-
-print("¿El pokemon a criar lo quieres con una naturaleza específica?: \n")
-naturaleza = int(input("Si (1 \nNo (2 \n:"))
+print("Do you want the bred Pokemon to have a specific nature?\n")
+nature = int(input("Yes (1)\nNo (2)\n:"))
 
 import math
 
-if naturaleza == 1:
-    print("¿Vas a criar un pokemon de una especie rara o con una especie común?: \n")
-    poke_raro = int(input("especie rara (1 \nespecie común (2 \n:"))
+if nature == 1:
+    print("Are you breeding a rare species or a common species?\n")
+    rare_pokemon = int(input("Rare species (1)\nCommon species (2)\n:"))
 
-    
-    if poke_raro == 1:
-        sexo_1 = int(input("Introduce el precio de elegir el género del breeder: "))
-        sexo_2 = int(input("Introduce el precio de elegir el género del pokemon: "))
-        piedraeterna = int(input("Introduce el precio de la piedraeterna: "))
-        cant_est = int(input("¿De cuantas estadísticas quieres a tu cría? (2-6): "))
-    
-        if cant_est >= 2 and cant_est <= 6:
-            cant_poke = int(2 ** (cant_est - 1) * 2)
-            cost_brazales = int((10000 * (math.log2(cant_poke) * 2)) - (cant_est * 10000))
-            cost_sexo = int((sexo_1 * (cant_poke / (2**(cant_est - 1)))) - (cant_est * sexo_1) + (cant_est * sexo_2))
-            piedraeterna = cant_est * piedraterna
-            cost_total =int(cost_brazales + cost_sexo + piedraeterna)
-            print("La cantidad de pokemones que necesitas es:", cant_poke)
-            print("El costo total para la cría que quieres es de: $", cost_total)
-            print("$", cost_sexo, "para elegir el sexo de la cría")
-            print("$", cost_brazales, "para los 'Power Item' y $", piedraeterna)
-            
+    if rare_pokemon == 1:
+        gender_1 = int(input("Enter the cost of choosing the breeder's gender: "))
+        gender_2 = int(input("Enter the cost of choosing the Pokemon's gender: "))
+        everstone = int(input("Enter the cost of the Everstone: "))
+        num_stats = int(input("How many stats do you want in your offspring? (2-6): "))
+
+        if 2 <= num_stats <= 6:
+            num_pokemon = int(2 ** (num_stats - 1) * 2)
+            cost_power_items = int((10000 * (math.log2(num_pokemon) * 2)) - (num_stats * 10000))
+            cost_gender = int((gender_1 * (num_pokemon / (2**(num_stats - 1)))) - (num_stats * gender_1) + (num_stats * gender_2))
+            everstone_cost = num_stats * everstone
+            total_cost = int(cost_power_items + cost_gender + everstone_cost)
+            print("The number of Pokemon you need is:", num_pokemon)
+            print("The total cost for the desired breeding is: $", total_cost)
+            print("$", cost_gender, "to choose the offspring's gender")
+            print("$", cost_power_items, "for the Power Items and $", everstone_cost)
+
         else:
-            print("El número introducido no es aceptable.")
+            print("The entered number is not acceptable.")
 
-            
-    elif poke_raro == 2:
-        sexo_1 = int(input("Introduce el precio de elegir el género del breeder: "))
-        sexo_2 = 0
-        piedraeterna = int(input("Introduce el precio de la piedraeterna: "))
-        cant_est = int(input("¿De cuantas estadísticas quieres a tu cría? (2-6): "))
-    
-        if cant_est >= 2 and cant_est <= 6:
-            cant_poke = int(2 ** (cant_est - 1) * 2)
-            cost_brazales = int(10000 * (math.log2(cant_poke) * 2) - (cant_est * 10000))
-            cost_sexo = int((sexo_1 * (cant_poke / (2**(cant_est - 1)))))
-            cost_total =int(cost_brazales + cost_sexo + piedraeterna)
-            print("La cantidad de pokemones que necesitas es:", cant_poke)
-            print("El costo total para la cría que quieres es de: $", cost_total)
-            print("$", cost_sexo, "para elegir el sexo de la cría")
-            print("$", cost_brazales, "para los 'Power Item' y $", piedraeterna)
-            
+    elif rare_pokemon == 2:
+        gender_1 = int(input("Enter the cost of choosing the breeder's gender: "))
+        gender_2 = 0
+        everstone = int(input("Enter the cost of the Everstone: "))
+        num_stats = int(input("How many stats do you want in your offspring? (2-6): "))
+
+        if 2 <= num_stats <= 6:
+            num_pokemon = int(2 ** (num_stats - 1) * 2)
+            cost_power_items = int(10000 * (math.log2(num_pokemon) * 2) - (num_stats * 10000))
+            cost_gender = int((gender_1 * (num_pokemon / (2**(num_stats - 1)))))
+            total_cost = int(cost_power_items + cost_gender + everstone)
+            print("The number of Pokemon you need is:", num_pokemon)
+            print("The total cost for the desired breeding is: $", total_cost)
+            print("$", cost_gender, "to choose the offspring's gender")
+            print("$", cost_power_items, "for the Power Items and $", everstone)
+
         else:
-            print("El número introducido no es aceptable.")
+            print("The entered number is not acceptable.")
 
-            
     else:
-        print("Esa opción no está disponible") 
+        print("That option is not available")
 
+elif nature == 2:
+    print("Are you breeding a rare species or a common species?\n")
+    rare_pokemon = int(input("Rare species (1)\nCommon species (2)\n:"))
+    if rare_pokemon == 1:
+        gender_1 = int(input("Enter the cost of choosing the breeder's gender: "))
+        gender_2 = int(input("Enter the cost of choosing the Pokemon's gender: "))
+        num_stats = int(input("How many stats do you want in your offspring? (2-6): "))
 
-elif naturaleza == 2:
-    print("¿Vas a criar un pokemon de una especie rara o con una especie común?: \n")
-    poke_raro = int(input("especie rara (1 \nespecie común (2 \n:"))
-    if poke_raro == 1:
-        sexo_1 = int(input("Introduce el precio de elegir el género del breeder: "))
-        sexo_2 = int(input("Introduce el precio de elegir el género del pokemon: "))
-        cant_est = int(input("¿De cuantas estadísticas quieres a tu cría? (2-6): "))
-    
-        if cant_est >= 2 and cant_est <= 6:
-            cant_poke = int(2 ** (cant_est - 2) * 2)
-            cost_brazales = int(10000 * (math.log2(cant_poke) * 2))
-            cost_sexo = int((sexo_1 * (cant_poke / (2**(cant_est - 1)))) - (cant_est * sexo_1) + (cant_est * sexo_2))
-            cost_total =int(cost_brazales + cost_sexo)
-            print("La cantidad de pokemones que necesitas es:", cant_poke)
-            print("El costo total para la cría que quieres es de: $", cost_total)
-            print("$", cost_sexo, "para elegir el sexo de la cría")
-            print("$", cost_brazales, "para los 'Power Item'")
+        if 2 <= num_stats <= 6:
+            num_pokemon = int(2 ** (num_stats - 2) * 2)
+            cost_power_items = int(10000 * (math.log2(num_pokemon) * 2))
+            cost_gender = int((gender_1 * (num_pokemon / (2**(num_stats - 1)))) - (num_stats * gender_1) + (num_stats * gender_2))
+            total_cost = int(cost_power_items + cost_gender)
+            print("The number of Pokemon you need is:", num_pokemon)
+            print("The total cost for the desired breeding is: $", total_cost)
+            print("$", cost_gender, "to choose the offspring's gender")
+            print("$", cost_power_items, "for the Power Items")
+
         else:
-            print("El número introducido no es aceptable.")
+            print("The entered number is not acceptable.")
 
+    elif rare_pokemon == 2:
+        gender_1 = int(input("Enter the cost of choosing the breeder's gender: "))
+        gender_2 = 0
+        num_stats = int(input("How many stats do you want in your offspring? (2-6): "))
 
+        if 2 <= num_stats <= 6:
+            num_pokemon = int(2 ** (num_stats - 2) * 2)
+            cost_power_items = int(10000 * (math.log2(num_pokemon) * 2))
+            cost_gender = int((gender_1 * (num_pokemon / (2**(num_stats - 1)))))
+            total_cost = int(cost_power_items + cost_gender)
+            print("The number of Pokemon you need is:", num_pokemon)
+            print("The total cost for the desired breeding is: $", total_cost)
+            print("$", cost_gender, "to choose the offspring's gender")
+            print("$", cost_power_items, "for the Power Items")
 
-    elif poke_raro == 2:
-        sexo_1 = int(input("Introduce el precio de elegir el género del breeder: "))
-        sexo_2 = 0
-        cant_est = int(input("¿De cuantas estadísticas quieres a tu cría? (2-6): "))
-    
-        if cant_est >= 2 and cant_est <= 6:
-            cant_poke = int(2 ** (cant_est - 2) * 2)
-            cost_brazales = int(10000 * (math.log2(cant_poke) * 2))
-            cost_sexo = int((sexo_1 * (cant_poke / (2**(cant_est - 1)))))
-            cost_total =int(cost_brazales + cost_sexo)
-            print("La cantidad de pokemones que necesitas es:", cant_poke)
-            print("El costo total para la cría que quieres es de: $", cost_total)
-            print("$", cost_sexo, "para elegir el sexo de la cría")
-            print("$", cost_brazales, "para los 'Power Item'")
         else:
-            print("El número introducido no es aceptable.")
-        
+            print("The entered number is not acceptable.")
+
     else:
-        print("Esa opción no está disponible") 
+        print("That option is not available")
 
-
-    
 else:
-    print("Esa opción no está disponible")
+    print("That option is not available")
 
-print("Fin.")
+print("End.")
